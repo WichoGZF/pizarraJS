9b4854115dd751116022440175af653bac5c8ad9
WichoGZF <wichogzf@gmail.com>
2023-03-06 13:39:53 -0600

    added proper player pos initialization, insertion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css'
-import { setupCounter } from './counter'
 import { header } from './components/header/header'
 import { field } from './components/field/field'
 import Position from '@customTypes/Position'
 import Player from '../src/types/Player'
+import createPlayerEl from './components/field/player/createPlayerEl'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ${header}  
@@ -15,8 +15,8 @@ interface Team {
   players: Player[];
   show: boolean;
   initialPos: Position[];
-  setInitialPos(arg1:number , arg2: number): void; 
-  setShow(): void; 
+  setInitialPos(arg1: number, arg2: number): void;
+  setShow(): void;
   hide(): void;
 }
 
@@ -28,30 +28,31 @@ class RedTeam implements Team {
   constructor(element: HTMLElement) {
     this.show = true;
 
-    const widthUnit = element.clientWidth;
-    const heightUnit = element.clientHeight;
+    const widthUnit = Math.trunc(element.clientWidth/14);
+    const heightUnit = Math.trunc(element.clientHeight/5);
+    debugger
 
     this.setInitialPos(widthUnit, heightUnit);
-
+    
     for (let i = 0; i < 11; i++) {
       this.players[i] = new Player(this.initialPos[i], i + 1, false)
     }
 
   }
-  
+
   setInitialPos(widthUnit: number, heightUnit: number) {
     this.initialPos = [
-      { x: 1 * widthUnit, y: 3 * heightUnit},
-      { x: 2 * widthUnit, y: 2 * heightUnit},
-      { x: 2 * widthUnit, y: 4 * heightUnit},
-      { x: 3 * widthUnit, y: 1 * heightUnit},
-      { x: 3 * widthUnit, y: 5 * heightUnit},
-      { x: 4 * widthUnit, y: 3 * heightUnit},
-      { x: 5 * widthUnit, y: 2 * heightUnit},
-      { x: 5 * widthUnit, y: 4 * heightUnit},
-      { x: 6 * widthUnit, y: 1 * heightUnit},
-      { x: 6 * widthUnit, y: 5 * heightUnit},
-      { x: 7 * widthUnit, y: 3 * heightUnit},
+      { x: 1 * widthUnit, y: 3 * heightUnit },
+      { x: 2 * widthUnit, y: 2 * heightUnit },
+      { x: 2 * widthUnit, y: 4 * heightUnit },
+      { x: 3 * widthUnit, y: 1 * heightUnit },
+      { x: 3 * widthUnit, y: 5 * heightUnit },
+      { x: 4 * widthUnit, y: 3 * heightUnit },
+      { x: 5 * widthUnit, y: 2 * heightUnit },
+      { x: 5 * widthUnit, y: 4 * heightUnit },
+      { x: 6 * widthUnit, y: 1 * heightUnit },
+      { x: 6 * widthUnit, y: 5 * heightUnit },
+      { x: 7 * widthUnit, y: 3 * heightUnit },
     ]
   }
   setShow() {
@@ -71,8 +72,8 @@ class BlueTeam implements Team {
   constructor(element: HTMLElement) {
     this.show = true;
 
-    const widthUnit = element.clientWidth;
-    const heightUnit = element.clientHeight;
+    const widthUnit = Math.round(element.clientWidth/14);
+    const heightUnit = Math.round(element.clientHeight/5);
 
     this.setInitialPos(widthUnit, heightUnit);
 
@@ -113,3 +114,15 @@ const fieldElement: HTMLElement = document.querySelector<HTMLElement>('#field')!
 const redTeam = new RedTeam(fieldElement)
 const blueTeam = new BlueTeam(fieldElement)
 
+///inserting initial players 
+const fragment = document.createDocumentFragment();
+
+redTeam.players.forEach((player: Player, index: number) => {
+  fragment.appendChild(createPlayerEl(player.number, 'red', player.position))
+})
+
+blueTeam.players.forEach((player:Player, index: number) => {
+  fragment.appendChild(createPlayerEl(player.number, 'blue', player.position))
+})
+
+fieldElement.append(fragment)
