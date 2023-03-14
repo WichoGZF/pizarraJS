914ba740f85b38e097820f9f18efac18444c18d7
WichoGZF <wichogzf@gmail.com>
2023-03-13 22:19:45 -0600

    Fixed draw buttons now working

--- src/components/field/field.ts
+++ src/components/field/field.ts
@@ -4,19 +4,17 @@ export const field: string = `
     <div class=${styles.field}>
         <div class=${styles.bar}>
             <div class=${styles.group}>
-                <input type='checkbox' id='pencil' class=${styles.checkbox} ></input>
-                <label htmlFor='pencil' class=${styles.pencilLabel}></label>
-                <input type='checkbox' id='eraser' class=${styles.checkbox} ${styles.eraser}></input>
-                <label htmlFor='eraser' class=${styles.eraserLabel}></label>
+                <input type='checkbox' id='pencil' class="${styles.checkbox} ${styles.pencil}"></input>
+                <input type='checkbox' class="${styles.checkbox} ${styles.eraser}"></input>
                 <input type='color'></input>
                 <button id="capture" class=${styles.capture}></button>
             </div>
             <div class=${styles.group}>
-                <button id="reset-red" class=${styles.reset} ${styles.red}></button>
-                <button id="hide-red" class=${styles.hide} ${styles.red}></button>
+                <button id="reset-red" class="${styles.reset} ${styles.red}"></button>
+                <button id="hide-red" class="${styles.hide} ${styles.red}"></button>
                 <div class=${styles.divider}></div>
-                <button id="reset-blue" class=${styles.reset} ${styles.blue}></button>
-                <button id="hide-blue" class=${styles.hide} ${styles.blue}></button>
+                <button id="reset-blue" class="${styles.reset} ${styles.blue}"></button>
+                <button id="hide-blue" class="${styles.hide} ${styles.blue}"></button>
             </div>
         </div>
         <div id="field" class=${styles.playField}>
